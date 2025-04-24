50786df4bb7adeb75fb598995cf2e05690a1de89
Gene Boggs <gene@ology.net>
2025-04-24 11:12:43 -0700

    WIP

--- lib/MIDI/Device.pm
+++ lib/MIDI/Device.pm
@@ -26,45 +26,45 @@ sub transmits {
 
 These are the MIDI control change messages recognized generically:
 
-    0 => 'Bank Select',
-    1 => 'Modulation',
-    2 => 'Breath Controller',
-    4 => 'Foot Controller',
-    5 => 'Portamento Time',
-    6 => 'Data Entry (MSB)',
-    7 => 'Volume',
-    8 => 'Balance', # 0 = hard left, 64 = center, 127 = hard right
-   10 => 'Pan',     # 0 = hard left, 64 = center, 127 = hard right
-   11 => 'Expression',
-   12 => 'Effect Controller 1',
-   13 => 'Effect Controller 2',
-   64 => 'Damper / Sustain Pedal',   # ≤63 off, ≥64 on
-   65 => 'Portamento On/Off',        # ≤63 off, ≥64 on
-   66 => 'Sostenuto On/Off',         # ≤63 off, ≥64 on
-   67 => 'Soft Pedal On/Off',        # ≤63 off, ≥64 on
-   68 => 'Legato FootSwitc	On/Off', # ≤63 Normal, ≥64 Legato
-   69 => 'Hold 2',
-   70 => 'Sound Variation',
-   71 => 'Resonance',
-   72 => 'Release Time',
-   73 => 'Attack',
-   74 => 'Cutoff frequency',
-   84 => 'Portamento CC Control',
-   91 => 'Reverb send amount',
-   92 => 'Tremolo amount',
-   93 => 'Chorus amount',
-   94 => 'Detune amount',
-   95 => 'Phaser amount',
-   96 => 'Data Increment',
-   97 => 'Data Decrement',
-  120 => 'All Sound Off',
-  121 => 'Reset All Controllers',
-  122 => 'Local On/Off',
-  123 => 'All Notes Off',
-  124 => 'Omni Mode Off',
-  125 => 'Omni Mode On',
-  126 => 'Mono Mode',
-  127 => 'Poly Mode',
+    0 Bank Select
+    1 Modulation # vibrato effect (pitch, loudness, brighness)
+    2 Breath Controller
+    4 Foot Controller
+    5 Portamento Time
+    6 Data Entry (MSB)
+    7 Volume
+    8 Balance # 0 = hard left, 64 = center, 127 = hard right
+   10 Pan     # 0 = hard left, 64 = center, 127 = hard right
+   11 Expression', # percentage of volume
+   12 Effect Controller 1
+   13 Effect Controller 2
+   64 Damper / Sustain Pedal   # ≤63 off, ≥64 on
+   65 Portamento On/Off        # ≤63 off, ≥64 on
+   66 Sostenuto On/Off         # ≤63 off, ≥64 on
+   67 Soft Pedal On/Off        # ≤63 off, ≥64 on
+   68 Legato FootSwitc	On/Off # ≤63 Normal, ≥64 Legato
+   69 Hold 2
+   70 Sound Variation
+   71 Resonance
+   72 Release Time
+   73 Attack
+   74 Cutoff frequency
+   84 Portamento CC Control
+   91 Reverb send amount
+   92 Tremolo amount
+   93 Chorus amount
+   94 Detune amount
+   95 Phaser amount
+   96 Data Increment
+   97 Data Decrement
+  120 All Sound Off
+  121 Reset All Controllers
+  122 Local On/Off
+  123 All Notes Off
+  124 Omni Mode Off
+  125 Omni Mode On
+  126 Mono Mode
+  127 Poly Mode
 
 =cut
 
